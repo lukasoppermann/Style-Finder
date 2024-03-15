f2aa7ba7b4bb1140557313a692f1c034bd531148
Lukas Oppermann <lukasoppermann@github.com>
2024-03-15 15:20:37 +0100

    performance improvements

--- src/utilities/getNodesWithStyles.ts
+++ src/utilities/getNodesWithStyles.ts
@@ -1,21 +1,27 @@
 import { NodeWithStyle, hasEffectStyle, hasFillStyle, hasGridStyle, hasTextStyle, hasStrokeStyle } from './hasStyle';
 import { Settings } from './settings';
 
-export const getNodesWithStyles = (parent: PageNode | DocumentNode, settings: Settings): NodeWithStyle[] => parent.findAllWithCriteria({
-  types: ["BOOLEAN_OPERATION",
-    "COMPONENT",
-    "COMPONENT_SET",
-    "ELLIPSE",
-    "FRAME",
-    "INSTANCE",
-    "LINE",
-    "POLYGON",
-    "RECTANGLE",
-    "SHAPE_WITH_TEXT",
-    "STAMP",
-    "STAR",
-    "STICKY",
-    "TEXT",
-    "VECTOR",]
-})
-  .filter(n => (settings.SHOW_PAINT && hasFillStyle(n)) || (settings.SHOW_EFFECT && hasEffectStyle(n)) || (settings.SHOW_TEXT && hasTextStyle(n)) || (settings.SHOW_GRID && hasGridStyle(n)) || (settings.SHOW_PAINT && hasStrokeStyle(n))) as NodeWithStyle[];
+const isValidNode = (node: SceneNode): node is NodeWithStyle => ["BOOLEAN_OPERATION",
+  "COMPONENT",
+  "COMPONENT_SET",
+  "ELLIPSE",
+  "FRAME",
+  "INSTANCE",
+  "LINE",
+  "POLYGON",
+  "RECTANGLE",
+  "SHAPE_WITH_TEXT",
+  "STAMP",
+  "STAR",
+  "STICKY",
+  "TEXT",
+  "VECTOR",].includes(node.type)
+
+const hasRequestStyles = (n: NodeWithStyle, settings: Settings) =>
+  (settings.SHOW_PAINT && hasFillStyle(n))
+  || (settings.SHOW_EFFECT && hasEffectStyle(n))
+  || (settings.SHOW_TEXT && hasTextStyle(n))
+  || (settings.SHOW_GRID && hasGridStyle(n))
+  || (settings.SHOW_PAINT && hasStrokeStyle(n))
+
+export const getNodesWithStyles = (parent: PageNode | DocumentNode, settings: Settings): NodeWithStyle[] => parent.findAll((n) => isValidNode(n) && hasRequestStyles(n, settings)) as NodeWithStyle[]
